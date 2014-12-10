2afaa4da4a801751410743831fe4f9c0c2ff151b
fewstera <aidan.fewster@bbc.co.uk>
2014-12-10 11:28:11 +0000

    Fixed font sizes and hide main until load

--- newsspec_9554/source/js/app.js
+++ newsspec_9554/source/js/app.js
@@ -2,7 +2,7 @@ define(['lib/news_special/bootstrap', 'countrySelectController'], function (news
 
     return {
         init: function () {
-
+            news.$('.main').show();
             new CountrySelectController();
             news.sendMessageToremoveLoadingImage();
 
